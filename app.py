66e4f54c910144f553172f7e608efad06b8bc47d
1Deekshitha <deeksh2602@gmail.com>
2025-10-02 13:41:25 +0530

    comm

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ from sklearn.preprocessing import LabelEncoder
 from flask import Flask, render_template, request
 
 from sklearn.ensemble import RandomForestClassifier
-
-
-
-
+import os
 app = Flask(__name__, template_folder='templates')  # Specify the directory for templates
 
 # Load dataset
@@ -68,5 +65,8 @@ def login():
 
     return render_template('login.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
